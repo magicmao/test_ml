eb348917db5497f2d278b764833ae5c7fb52e9f6
ubuntu <ubuntu@localhost.localdomain>
2020-12-05 15:04:56 +0800

    update

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import fastbook
+fastbook.setup_book()
+
+from fastbook import *
 from flask import Flask, request, jsonify
 #from fastai.basic_train import load_learner
 #from fastai.vision.widgets import *
+from fastai import *
+from fastai.learner import *
+from fastai.vision.all import *
 from fastai.vision.widgets import *
 from fastai.layers import *
 from fastai.torch_core import *
@@ -9,8 +16,8 @@ app = Flask(__name__)
 CORS(app, support_credentials=True)
 
 # load the learner
-learn = load_learner(path='./models', file='trained_model.pkl', cpu=True)
-classes = learn.data.classes
+learn = load_learner('trained_model.pkl', cpu=True)
+classes = learn.dls[1].vocab
 
 
 def predict_single(img_file):
